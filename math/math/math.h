569c23c603ecfe3041a0b6dd213514c9d372e4ee
Ruslan Popesku <ruslanpopesku95@gmail.com>
2017-11-21 14:48:51 +0200

    no message

---
+++ math/math/math.h
@@ -0,0 +1,19 @@
+//
+//  math.h
+//  math
+//
+//  Created by Admin on 11/21/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for math.
+FOUNDATION_EXPORT double mathVersionNumber;
+
+//! Project version string for math.
+FOUNDATION_EXPORT const unsigned char mathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <math/PublicHeader.h>
+
+
---
+++ math/math/math.swift
@@ -0,0 +1,19 @@
+//
+//  math.swift
+//  math
+//
+//  Created by Admin on 11/21/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import Foundation
+
+public class Math {
+    public var a: Int!
+    init(a: Int) {
+        self.a = a
+    }
+    public func accurate() {
+        print("as;ldfk;alsdkfj;ladskjf;lkasjdfjads;lfjas;lfjas;dkjf;laskjdf;")
+    }
+}
